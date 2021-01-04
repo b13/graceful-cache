a4aa8fc3d147f0c2ff3ea2380aa98761b3ecb258
Benni Mack <benni@typo3.org>
2021-01-04 16:33:51 +0100

    [TASK] Add more wrapper methods

--- src/Backend/RedisCacheBackend.php
+++ src/Backend/RedisCacheBackend.php
@@ -75,4 +75,48 @@ class RedisCacheBackend extends RedisBackend implements LoggerAwareInterface
         }
         return [];
     }
+
+    public function set($entryIdentifier, $data, array $tags = [], $lifetime = null)
+    {
+        if ($this->connected) {
+            try {
+                parent::set($entryIdentifier, $data, $tags, $lifetime);
+            } catch (\Throwable $e) {
+                $this->logger->error('Error while setting data into Redis Cache', [
+                    'message' => $e->getMessage(),
+                    'error' => $e
+                ]);
+            }
+        }
+    }
+
+    public function remove($entryIdentifier)
+    {
+        if ($this->connected) {
+            try {
+                return parent::remove($entryIdentifier);
+            } catch (\Throwable $e) {
+                $this->logger->error('Error while removing data from Redis Cache', [
+                    'message' => $e->getMessage(),
+                    'error' => $e
+                ]);
+            }
+        }
+        return false;
+    }
+
+    public function collectGarbage()
+    {
+        if ($this->connected) {
+            try {
+                parent::collectGarbage();
+            } catch (\Throwable $e) {
+                $this->logger->error('Error while collecting garbage in Redis Cache', [
+                    'message' => $e->getMessage(),
+                    'error' => $e
+                ]);
+            }
+        }
+    }
 }
+
